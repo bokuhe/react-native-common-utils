c1acec19f3125096639e0ae82e649282b5343205
H. Park <bokuhe@gmail.com>
2023-08-28 19:06:07 +0900

    chore: add lib

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,6 @@
+export interface AppNavigatorType {
+    backToPreviousApp: () => Promise<void>;
+    exitApp: () => Promise<void>;
+}
+export declare const AppNavigator: AppNavigatorType;
+//# sourceMappingURL=index.d.ts.map
